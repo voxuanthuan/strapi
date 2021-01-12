93c28a6edd0aa8f60ec9b83dd5033fe5d45befc5
Xuan Thuan <voxuanthuan280194@gmail.com>
2021-01-12 17:57:27 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fe7ec640a4920c2a826cc8cc609889e4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f12d4353-5369-4836-9b66-7ff5149c3ab7'
+};
